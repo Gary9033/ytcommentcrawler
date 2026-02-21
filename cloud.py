d3de5d495acf3c74cc39243d93646068e9fa0fbd
Gary9033 <B1029033@cgu.edu.tw>
2026-02-21 18:06:20 +0800

    final

--- cloud.py
+++ cloud.py
@@ -18,7 +18,7 @@ plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei', 'DejaVu Sans']
 plt.rcParams['axes.unicode_minus'] = False
 
 # --- 設定區 ---
-API_KEY = ''
+API_KEY = 'AIzaSyCEzWvL9Zg6HXcPnC94JM_yA6ueP0trFWY'
 
 def get_video_id(url):
     if 'shorts/' in url:
